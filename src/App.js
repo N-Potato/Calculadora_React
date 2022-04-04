518ef838afd097999921dfd4e597b48fbd38d5ec
Tiago de Andrade Valim <tiago.a.valim@gmail.com>
2022-04-04 15:19:41 -0300

    calculadora

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
 
 function App() {
+  const [n1, setN1] = useState(0)
+  const [n2, setN2] = useState(0)
+  const [opcao, setOpcao] = useState('Somar')
+  const [resultado, setResultado] = useState(0)
+
+  const calcular = () => {
+    if (opcao === 'Somar')
+      return parseFloat(n1) + parseFloat(n2)
+    else if (opcao === 'Subtrair')
+      return parseFloat(n1) - parseFloat(n2)
+    else if (opcao === 'Multiplicar')
+      return parseFloat(n1) * parseFloat(n2)
+    else if (opcao === 'Dividir')
+      return parseFloat(n1) / parseFloat(n2)
+
+  }
+
+    useEffect(() =>{
+      setResultado(calcular())
+    }, [n1, n2, opcao ])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Calculadora</h1>
+
+        <label>Primeiro Número</label>
+        <input type="number"
+          value={n1}
+          onChange={e => setN1(e.target.value)}
+        />
+
+        <label>Segundo Número</label>
+        <input type="number"
+          value={n2}
+          onChange={e => setN2(e.target.value)}
+        />
+
+        <select onChange={e => setOpcao(e.target.value)}>
+          <option>Somar</option>
+          <option>Subtrair</option>
+          <option>Multiplicar</option>
+          <option>Dividir</option>
+        </select>
+
+        <label>Resultado {resultado}</label>
+
       </header>
     </div>
   );
